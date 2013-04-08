d4aaa97ab16ff8c7399e17b0ebcdd14b52b6570b
Chaolun Xia <sasnzy13@163.com>
2013-04-08 17:15:15 -0400

    1

---
+++ distributed_gp/utility/photo_transfer.py
@@ -0,0 +1,48 @@
+from event_interface import EventInterface
+from event_feature import EventFeature
+from event_feature_sparse import EventFeatureSparse
+from photo_interface import PhotoInterface
+from photo import Photo
+from region import Region
+from event import Event
+from caption_parser import CaptionParser
+from stopwords import Stopwords
+from bson.objectid import ObjectId
+from corpus import Corpus
+from representor import Representor
+
+import operator
+import string
+import types
+import random
+import math
+
+import sys
+
+def transferPhoto():
+	# remove duplicate
+	pi = PhotoInterface()
+	pi.setDB('citybeat')
+	pi.setCollection('photos')
+	
+	pi2 = PhotoInterface()
+	pi2.setDB('citybeat_production')
+	pi2.setCollection('photos')
+	
+	photo_cur = pi.getAllDocuments()
+	id_set = set()
+	for photo in photo_cur:
+		id_set.add(photo['id'])
+		photo['_id'] = photo['id']
+		pi2.saveDocument(photo)
+
+if __name__=='__main__':
+	main()
+	
+	
+	
+	
+	
+	
+	
+	
