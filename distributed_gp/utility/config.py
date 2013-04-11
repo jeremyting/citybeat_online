47707a0f3770a5e27fa441d4d519b367158c2c6b
Chaolun Xia <sasnzy13@163.com>
2013-04-11 18:04:35 -0400

    1

--- distributed_gp/utility/config.py
+++ distributed_gp/utility/config.py
@@ -43,15 +43,6 @@ class InstagramConfig(BaseConfig):
     #region_M = 25
     region_percentage = 0.3
 
-instagram_client_id = '4d9231b411eb4ef69435b40eb83999d6'
-instagram_client_secret = '204c565fa1244437b9034921e034bdd6'
-
-instagram_API_pause = 0.1
-
-#mongodb_address = 'grande'
-mongodb_address = 'grande.rutgers.edu'
-mongodb_port = 27017
-
 class TwitterConfig(BaseConfig):
 	# we have not yet moved tweets from citybeat to production
 	tweet_db = 'citybeat_production'
