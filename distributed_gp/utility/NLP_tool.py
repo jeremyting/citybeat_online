fc027860175fd7b78e49768638c529e07a5f747b
Chaolun Xia <sasnzy13@163.com>
2013-04-15 17:13:14 -0400

    1

--- distributed_gp/utility/NLP_tool.py
+++ distributed_gp/utility/NLP_tool.py
@@ -1,5 +1,8 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-def parseText(text):
+
+def textProprocessor(text):
 		
 	def removeAt(text):
 		# remove @xxx
@@ -31,19 +34,20 @@ def parseText(text):
 		else:
 			new_text += ' '
 		pre_is_text = False
-		 
-	words = new_text.split()
-	tmp_dict = {} 
 	
-	for word in words:
-		word = word.strip()
-		if len(word) >= 3:
-			tmp_dict[word] = tmp_dict.get(word, 0) + 1
-	
-	return tmp_dict
+	return new_text.strip()
 		
 if __name__ == '__main__':
 	text1 = 'gfd #@ @xia@2b #xcv@xcb hahasb@bbb gfd #@ @xia@2b #xcv@xcb hahasb@bbb gfd #@ @xia@2b #xcv@xcb hahasb@bbb'
 	text2 = 'YousbLoveMesb'
-	a = parseText
+	text3 = 'YousbLoveMesb gdf'
+	a = textProprocessor
 	print a(text2)
+	
+	vectorizer = TfidfVectorizer( max_df=0.99, min_df=0, strip_accents='ascii', smooth_idf=True,	
+																preprocessor=textProprocessor, sublinear_tf=True, norm='l2', 
+																analyzer='word', ngram_range=(1,1), stop_words = 'english')
+	
+	documents = [text1, text2, text2, text3]
+	print vectorizer.fit_transform(documents)
+	print vectorizer.get_feature_names()
