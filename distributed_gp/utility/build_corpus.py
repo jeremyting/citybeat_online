1b00d6e464153fe1732e6952468cbb8c0c49544d
Chaolun Xia <sasnzy13@163.com>
2013-04-16 19:50:11 -0400

    1

--- distributed_gp/utility/build_corpus.py
+++ distributed_gp/utility/build_corpus.py
@@ -13,40 +13,6 @@ import random
 import time
 from datetime import datetime
 
-def textProprocessor(text):
-		
-	def removeAt(text):
-		# remove @xxx
-		new_text = ''
-		for word in text.split(' '):
-			word = word.strip()
-			if word == '':
-				continue
-			if word.startswith('@'):
-				continue
-			new_text += word + ' '
-		return new_text.strip()
-		
-	text = removeAt(text)
-	# change the word YouLoveMe into you love me seperately
-	new_text = ''
-	pre_is_text = False
-	for c in text:
-		if c.isupper():
-			if not pre_is_text:
-				new_text += ' '
-			new_text += c.lower()
-			pre_is_text = True
-			continue
-			
-		if c.islower():
-			new_text += c
-		else:
-			new_text += ' '
-		pre_is_text = False
-		
-	new_text = removeAt(new_text)
-	return new_text.strip()
 
 def getTfidfVector(vectorizer, text):
 	voc = vectorizer.get_feature_names()
--- distributed_gp/utility/corpus.py
+++ distributed_gp/utility/corpus.py
@@ -2,60 +2,100 @@ from event_interface import EventInterface
 from caption_parser import CaptionParser
 from photo import Photo
 
+from photo_interface import PhotoInterface
+from tweet_interface import TweetInterface
+from photo import Photo
+from tweet import Tweet
+from region import Region
+from config import InstagramConfig
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import tool
+
+import random
+import time
+from datetime import datetime
+
+
 import time
 import math
 import operator
 
 class Corpus(object):
 	
-	def __init__(self, leastDF=5):
-		self._corpus_df = {}
-		self._corpus_n = 0
-		self._leastDF = leastDF
+	def getKey(self):
+		assert self._region not is None
+		r = Region(self._region)
+		return r.toJSON()
 		
-	def _addDocument(self, word_list):
-		for word, value in word_list:
-			if word in self._corpus_df:
-				self._corpus_df[word] += 1
-			else:
-				self._corpus_df[word] = 1
-		self._corpus_n += 1
-	
-	def getWordList(self, event):
-		# word_list is a list of (word, freq)
-		cp = CaptionParser(True)
-		for photo in event['photos']:
-			photo = Photo(photo)
-			cp.insertCaption(photo.getCaption())
-		return cp.getTopWords(-1, False)
-	
-	def buildCorpusOnDB(self, db, collection):
-		ei = EventInterface()
-		ei.setDB(db)
-		ei.setCollection(collection)
-		events = ei.getAllDocuments()
-		for event in events:
-			word_list = self.getWordList(event)
-			self._addDocument(word_list)
+	def _textProprocessor(text):
+		
+		def removeAt(text):
+			# remove @xxx
+			new_text = ''
+			for word in text.split(' '):
+				word = word.strip()
+				if word == '' or word.startswith('@'):
+					continue
+				new_text += word + ' '
+			return new_text.strip()
+		
+		text = removeAt(text)
+		# change the word YouLoveMe into you love me seperately
+		new_text = ''
+		pre_is_text = False
+		for c in text:
+			if c.isupper():
+				if not pre_is_text:
+					new_text += ' '
+				new_text += c.lower()
+				pre_is_text = True
+				continue
 			
-	def chooseTopWordWithHighestTDIDF(self, word_list, k=10):
-		# word_list is a list of (word, freq)
+			if c.islower():
+				new_text += c
+			else:
+				new_text += ' '
+			pre_is_text = False
+			
+		new_text = removeAt(new_text)
+		return new_text.strip()
 		
-		# TODO: temporally add this sentence to avoid error when query the df of new word
-		self._addDocument(word_list)
-		
-		new_word_list = []
-		for i in xrange(0, len(word_list)):
-			word = word_list[i][0]
-			tf = word_list[i][1]
-			# note that the corpus must be consistent with the events
-			# that means, we cannot use the corpus built from 15by15 for 10by10
-			df = self._corpus_df.get(word, 1)
-			if df >= self._leastDF:
-				tfidf = tf * math.log(self._corpus_n * 1.0 / df)
-				new_word_list.append((word, tfidf))
-		new_word_list.sort(key=operator.itemgetter(1), reverse=True)
-		return new_word_list[0:min(k, len(new_word_list))]
+	def buildCorpus(region, time_interval, document_type='photo'):
+		# time_interval should be [start, end]
+		if document_type == 'photo':
+			di = PhotoInterface()
+		else:
+			di = TweetInterface()
+		cur = di.rangeQuery(region, time_interval)
+		text = []
+		for document in cur:
+			if document_type == 'photo':
+				doc = Photo(document)
+			else:
+				doc = Tweet(document)
+			t = doc.getText()
+			#at least 5 length
+			if len(t) > 4:
+				text.append(t)
+		self._region = region
+		self._vectorizer = TfidfVectorizer(max_df=100, min_df=0, strip_accents='ascii', preprocessor=self._textProprocessor,
+		                             smooth_idf=True, sublinear_tf=True, norm='l2', 
+																 analyzer='word', ngram_range=(1,1), stop_words = 'english')
+		self._vectorizer.fit_transform(text)
+			
+	def chooseTopWordWithHighestTDIDF(self, text, k=10):
+			voc = vectorizer.get_feature_names()
+			tf_vec = vectorizer.transform([text]).mean(axis=0)
+			nonzeros = np.nonzero(tf_vec)[1]
+	res_list = nonzeros.ravel().tolist()[0] 
+	values = []
+	words = []
+	for n in res_list:
+		words.append( voc[n] )
+		values.append( tf_vec[0,n] )
+	return res_list, words, values
 			
 	
 if __name__ == '__main__':
