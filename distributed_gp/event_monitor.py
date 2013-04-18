cd45bee66f8d3900c548426567e5d4ea2d19febc
KE XIE <kx19@grande.rutgers.edu>
2013-04-18 15:41:38 -0400

    add all

--- distributed_gp/event_monitor.py
+++ distributed_gp/event_monitor.py
@@ -11,19 +11,24 @@ from utility.event_interface import EventInterface
 from utility.event_feature_instagram import EventFeatureInstagram
 from utility.corpus import buildAllCorpus
 
+
 class EventMonitor():
     def __init__(self, db, collection):
         self.db = db
         self.collection = collection
-        all_corpus = buildAllCorpus()
+        self.all_corpus = buildAllCorpus( time_interval_length = 2)
+                
 
     def goThroughCandidateDB(self):
         """Go through candidate event db and classify whatever is left"""
         ei = EventInterface(self.db, self.collection)
-        
+        print 'begin '
+        cnt = 0
         for e in ei.getAllDocuments():
+            print 'working on ',cnt
+            cnt+=1
             region = e['region']
-            corpus = all_corpus[region.getKey()]
+            corpus = self.all_corpus[region.getKey()]
             ef = EventFeatureInstagram(e, corpus)
             print ef.extractFeatures()
 
