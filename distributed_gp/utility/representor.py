63efbdc33cb1b8c7ef77185ba63b6442cb1a38b9
Chaolun Xia <sasnzy13@163.com>
2013-04-27 12:46:12 -0400

    1

--- distributed_gp/utility/representor.py
+++ distributed_gp/utility/representor.py
@@ -45,7 +45,7 @@ class Representor():
         paras['analyzer'] = 'char_wb'
         paras['ngram_range'] = (4,4)
         paras['stop_words'] = 'english'
-        self._corpus_dicts_char = buildAllCorpus(debug=True, element_type=self._element_type, paras=paras)
+        self._corpus_dicts_char = buildAllCorpus(element_type=self._element_type, paras=paras)
 
         #paras['analyzer'] = 'word'
         #paras['ngram_range'] = (1,1)
