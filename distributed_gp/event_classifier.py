1e618cee889ea3e99903548002a67bad82aa1c45
KE XIE <kx19@grande.rutgers.edu>
2013-04-28 17:20:33 -0400

    change event classifier

---
+++ distributed_gp/event_classifier.py
@@ -0,0 +1,73 @@
+
+#read tf-idf
+# while 
+#   search db
+#   if e not classified
+#       classify e. 
+#       if e is event:
+#           put it back to front-end db
+
+from utility.event_interface import EventInterface
+from utility.base_feature_production import BaseFeatureProduction
+from utility.corpus import buildAllCorpus
+from utility.region import Region
+from utility.event import Event
+import classifier
+import time
+
+class EventMonitor():
+    def __init__(self, candidate_db, candidate_collection, classified_event_db, classified_event_collection, event_backup_db, event_backup_collection):
+        self.candidate_db = candidate_db
+        self.candidate_collection = candidate_collection
+        self.classified_event_db = classified_event_db
+        self.classified_event_collection = classified_event_collection
+        self.event_backup_db = event_backup_db
+        self.event_backup_collection = event_backup_collection 
+
+        self.all_corpus = buildAllCorpus( time_interval_length = 14)
+        self.clf = classifier.Classifier() 
+
+    def goThroughCandidateDB(self):
+        """Go through candidate event db and classify whatever is left"""
+        ei = EventInterface(self.candidate_db, self.candidate_collection)
+        ei_classified = EventInterface(self.classified_event_db, self.classified_event_collection)
+        ei_backup = EventInterface(self.event_backup_db, self.event_backup_collection)
+        cnt = 0
+        
+        for e in ei.getAllDocuments():
+            e = Event(e)
+            print 'working on ',cnt
+            cnt+=1
+            region = Region(e.getRegion())
+            corpus = self.all_corpus[region.getKey()]
+            ef = BaseFeatureProduction(e, corpus)     
+            prob = self.clf.classify(ef.extractFeatures())
+            
+            ei_backup.addEvent(e)
+            ei.deleteEventByID(str(e.getID()))
+            if prob > 0.5:
+                print 'ready to insert'
+                e.setLabel( prob )
+                ei_classified.addEvent(e)
+            
+    def monitor(self):
+        em = EventMonitor('citybeat', 'candidate_event_15by15_merged',  'citybeat', 'instagram_front_end_events', 'citybeat', 'event_backup_instagram')
+        while True:
+            self.goThroughCandidateDB()
+            time.sleep(60)
+
+def test():
+    em = EventMonitor('citybeat_production', 'online_candidate_instagram',  'citybeat_production', 'instagram_front_end_events', 'citybeat_production', 'event_backup_instagram')
+    em.goThroughCandidateDB()
+
+def main():
+    em = EventMonitor('citybeat_production', 'online_candidate_instagram',  'citybeat_production', 'instagram_front_end_events', 'citybeat_production', 'event_backup_instagram')
+    while True:
+        em.goThroughCandidateDB()
+        print 'sleep 3 minutes'
+        time.sleep(60*3)
+
+if __name__=='__main__':
+    #test()
+    main()
+
---
+++ routine/rt_event_classifier.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+procs=`ps aux|grep event_classifier.py |grep -v grep | wc -l`
+
+echo $procs
+if [ $procs -gt 0 ]; then
+    echo "process running. exit."
+else
+    echo "no process running. starting event classifer."
+    (cd /res/users/kx19/citybeat_online/distributed_gp && nohup python /res/users/kx19/citybeat_online/distributed_gp/event_classifier.py >> /grande/local/kx19/citybeat_log/event.log & )
+    echo "done"
+fi
+
