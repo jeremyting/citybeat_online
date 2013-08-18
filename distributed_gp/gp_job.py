dc2359bafdbeac41ef514b2e6418b13aebcf6a4b
eddie.xie <oeddyo@gmail.com>
2013-08-18 10:52:45 -0400

    add

--- distributed_gp/gp_job.py
+++ distributed_gp/gp_job.py
@@ -90,7 +90,7 @@ class GaussianProcessJob():
         training, testing, align, converted = self._dataPrepare()
         result = self.q.enqueue_call(Predict, args=( training, testing, self._id,), timeout=86400, result_ttl=-1)
         logging.warning("Submitting job. Details as follow")
-        logging.warning("Align: " + '\n'.join(align))
-        logging.warning("Converted: " + '\n'.join(converted))
+        logging.warning("Align: " + '\n'.join([str(ele) for ele in align]))
+        logging.warning("Converted: " + '\n'.join([str(ele) for ele in converted]))
         return result, converted
 
