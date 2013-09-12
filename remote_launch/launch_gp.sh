32fa7642a2f925aacea08cef43814c24cc4ea757
CHAOLUN XIA <cx28@joust.cs.rutgers.edu>
2013-09-12 10:15:35 -0400

    add ; in sh

--- remote_launch/launch_gp.sh
+++ remote_launch/launch_gp.sh
@@ -6,6 +6,6 @@ COUNT="0"
 for i in ${workers[*]};
 do 
     echo "SSH to $i"
-    ssh $i "source ~/.bash_profile nohup python /grad/users/cx28/citybeat_online/distributed_gp/run_gp_worker.py > /.freespace/gp_
+    ssh $i "source ~/.bash_profile; nohup python /grad/users/cx28/citybeat_online/distributed_gp/run_gp_worker.py > /.freespace/gp_
 worker_log.txt 2> /dev/null < /dev/null &"
 done
