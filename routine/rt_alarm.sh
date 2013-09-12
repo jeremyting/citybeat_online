4f9291d34a3e3c8b317695acb2a554af2f0b037c
eddie.xie <oeddyo@gmail.com>
2013-09-12 11:38:38 -0400

    change whoami to expandable

--- routine/rt_alarm.sh
+++ routine/rt_alarm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date
-source /grad/users/whoami/.bash_profile
-(cd /grad/users/whoami/citybeat_online/distributed_gp && nohup python /grad/users/whoami/citybeat_online/distributed_gp/run_online_alarm.py instagram >> /.freespace/alarm_db_report.txt & )
+source /grad/users/$whoami/.bash_profile
+(cd /grad/users/$whoami/citybeat_online/distributed_gp && nohup python /grad/users/$whoami/citybeat_online/distributed_gp/run_online_alarm.py instagram >> /.freespace/alarm_db_report2.txt & )
