c837221e2968e2aee6a31772f90f96f23e7fbac7
CHAOLUN XIA <cx28@joust.cs.rutgers.edu>
2013-09-12 10:13:35 -0400

    modify bash script (instagram crawler and GP)

---
+++ remote_launch/run_all_machines.sh
@@ -0,0 +1,4 @@
+python remote_launch.py kill gp gp_host.txt
+python remote_launch.py launch gp gp_host.txt
+python remote_launch.py kill api api_host.txt
+python remote_launch.py launch api api_host.txt
