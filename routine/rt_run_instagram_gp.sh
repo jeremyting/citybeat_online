a7bca81fc32a6d30c9b0b2f934f6e8dc6fcece0c
xiachaolun <cx28@cs.rutgers.edu>
2013-09-18 15:27:52 -0400

    modify log file name

--- routine/rt_run_instagram_gp.sh
+++ routine/rt_run_instagram_gp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date
 source /grad/users/$USER/.bash_profile
-(cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/run_gp.py instagram > /.freespace/xia_log2 &)
+(cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/run_gp.py instagram > /.freespace/instagram_distributed_gp2.log &)
