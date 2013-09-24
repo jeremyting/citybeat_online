6addd2e84562c87782d08f3721c4a6c73b46d7a9
Chaolun Xia <sasnzy13@163.com>
2013-09-23 21:58:52 -0400

    add mongodb backup

---
+++ utility/mongodb_backup.py
@@ -0,0 +1,17 @@
+import pymongo
+
+
+source_mongodb_address = 'grande.rutgers.edu'
+source_mongodb_port = 27017
+source_connection = pymongo.Connection(source_mongodb_address, source_mongodb_port)
+
+target_mongodb_address = 'tall2.rutgers.edu'
+target_mongodb_port = 27017
+target_connection = pymongo.Connection(target_mongodb_address, target_mongodb_port)
+
+for db in source_connection.database_names():
+    print db
+    target_connection.copy_database(db, db, source_mongodb_address)
+    print 'finished'
+
+
