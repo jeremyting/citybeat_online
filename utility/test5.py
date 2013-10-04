575fee65827e201200f4b2509ea5ff01e9a3735d
Chaolun Xia <sasnzy13@163.com>
2013-10-03 22:27:23 -0400

    clean code 1

--- utility/test5.py
+++ utility/test5.py
@@ -1,18 +1,6 @@
-class f1(object):
-    def cal(self, a):
-        print self.cal2(a)
-
-    def cal2(self, a):
-        assert 2 == 1
-        return a
+from event_interface import EventInterface
 
 
-class f2(f1):
-    def cal2(self, a):
-        return a + 1
 
-
-if __name__ == '__main__':
-    f = f1()
-    f.cal(2)
-    print 'ok'
+def main():
+    ei = EventInterface()
