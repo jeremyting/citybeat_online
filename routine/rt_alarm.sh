497f1c236c3cc574e375080f528e6d0083143a22
Chaolun Xia <sasnzy13@163.com>
2013-10-09 00:58:01 -0400

    modify the log ( from ">>" to ">")

--- routine/rt_alarm.sh
+++ routine/rt_alarm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date
 source /grad/users/$USER/.bash_profile
-(cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/run_online_alarm.py instagram >> /.freespace/citybeat_logs/instagram_alarm & )
+(cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/run_online_alarm.py instagram > /.freespace/citybeat_logs/instagram_alarm & )
--- routine/rt_event_classifier.sh
+++ routine/rt_event_classifier.sh
@@ -8,7 +8,7 @@ if [ $procs -gt 0 ]; then
     echo "process running. exit."
 else
     echo "no process running. starting event classifer."
-    (cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/event_classifier.py >> /.freespace/citybeat_logs/instagram_alarm & )
+    (cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/event_classifier.py > /.freespace/citybeat_logs/instagram_alarm & )
     echo "done"
 fi
 
