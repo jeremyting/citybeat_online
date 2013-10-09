69a1dd20f029121024e56cf3b0cb3879e98ef8c9
Chaolun Xia <sasnzy13@163.com>
2013-10-09 00:59:02 -0400

    modify the log ( from ">>" to ">")

--- system_monitor/monitor.sh
+++ system_monitor/monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date
 source ~/.bash_profile
-(cd $HOME/citybeat_online/system_monitor && nohup python $HOME/citybeat_online/system_monitor/system_monitor.py  > /.freespace/rt_update.log & )
+(cd $HOME/citybeat_online/system_monitor && nohup python $HOME/citybeat_online/system_monitor/system_monitor.py  > /.freespace/citybeat_logs/rt_update.log & )
