f85258ae56ce211cfeacb3b7ca5fc33dcdcb311f
Chaolun Xia <sasnzy13@163.com>
2013-10-09 01:36:53 -0400

    db back up

--- utility/mongodb_backup.py
+++ utility/mongodb_backup.py
@@ -4,32 +4,36 @@ from mongodb_interface import MongoDBInterface
 source_mongodb_address = 'ec2-23-22-67-45.compute-1.amazonaws.com'
 source_mongodb_port = 27017
 source_connection = pymongo.Connection(source_mongodb_address, source_mongodb_port)
+source_connection['admin'].authenticate( 'admin', 'mediumdatarules')
 
 target_mongodb_address = 'grande.rutgers.edu'
 target_mongodb_port = 27017
 target_connection = pymongo.Connection(target_mongodb_address, target_mongodb_port)
 
-source_connection['admin'].authenticate( 'admin', 'mediumdatarules')
 print source_connection.database_names()
 print source_connection['citybeat_production'].collection_names()
+print target_connection['citybeat_production'].collection_names()
+
 
 for collection in source_connection['citybeat_production'].collection_names():
     print 'start collection: ' + collection
-    target_interface = MongoDBInterface(target_connection)
-    target_interface.setDB('citybeat_production')
-    target_interface.setCollection(collection)
-
-    source_interface = MongoDBInterface(source_connection)
+    source_interface = MongoDBInterface()
+    source_interface._connection = source_connection
     source_interface.setDB('citybeat_production')
     source_interface.setCollection(collection)
+
+    target_interface = MongoDBInterface()
+    target_interface._connection = target_connection
+    target_interface.setDB('citybeat_production')
+    target_interface.setCollection(collection)
 
     count = 0
     for e in source_interface.getAllDocuments():
         try:
-            target_mongodb_address.saveDocument(e)
+            target_interface.saveDocument(e)
             count += 1
         except Exception:
-            pass
+            print 'fail'
     print 'finish collection: ' + collection
     print 'new documents:' + str(count)
 print 'finished all'
