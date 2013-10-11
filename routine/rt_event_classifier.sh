3d0e4451acee99aae234a97074d9b761e67d724b
Eddie Xie <eddie@ip-10-85-130-224.localdomain>
2013-10-11 00:42:14 -0400

    classifier monitor

--- routine/rt_event_classifier.sh
+++ routine/rt_event_classifier.sh
@@ -2,13 +2,12 @@
 
 # This file is currently not being used at TA machines.
 procs=`ps aux|grep event_classifier.py |grep -v grep | wc -l`
-source #HOME/.bash_profile
 echo $procs
 if [ $procs -gt 0 ]; then
     echo "process running. exit."
 else
     echo "no process running. starting event classifer."
-    (cd $HOME/citybeat_online/distributed_gp && nohup python $HOME/citybeat_online/distributed_gp/event_classifier.py > /.freespace/citybeat_logs/instagram_alarm & )
+    (cd $HOME/citybeat_online/distributed_gp && nohup python $HOME/citybeat_online/distributed_gp/event_classifier.py > $HOME/citybeat_logs/classifier_monitor_log & )
     echo "done"
 fi
 
