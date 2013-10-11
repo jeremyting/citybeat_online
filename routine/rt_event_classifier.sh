e2e3d6364b93605a09397f8116bc06949904ec68
xiachaolun <cx28@cs.rutgers.edu>
2013-10-11 00:36:54 -0400

    update the classifier monitor

--- routine/rt_event_classifier.sh
+++ routine/rt_event_classifier.sh
@@ -2,13 +2,13 @@
 
 # This file is currently not being used at TA machines.
 procs=`ps aux|grep event_classifier.py |grep -v grep | wc -l`
-source /grad/users/$USER/.bash_profile
+source #HOME/.bash_profile
 echo $procs
 if [ $procs -gt 0 ]; then
     echo "process running. exit."
 else
     echo "no process running. starting event classifer."
-    (cd /grad/users/$USER/citybeat_online/distributed_gp && nohup python /grad/users/$USER/citybeat_online/distributed_gp/event_classifier.py > /.freespace/citybeat_logs/instagram_alarm & )
+    (cd $HOME/citybeat_online/distributed_gp && nohup python $HOME/citybeat_online/distributed_gp/event_classifier.py > /.freespace/citybeat_logs/instagram_alarm & )
     echo "done"
 fi
 
