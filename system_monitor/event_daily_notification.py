eda27df44bd75b34110f1c19e7b333f07f93bf8d
Chaolun Xia <sasnzy13@163.com>
2013-10-16 16:48:44 -0400

    add notification via email

--- system_monitor/event_daily_notification.py
+++ system_monitor/event_daily_notification.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
-import smtplib
+import smtplib, os
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
 
-def send_email(msg):
-    user = "xieke.buaa"
-    pw = "yjxkk131415"
+def send_email():
+    #user = "xieke.buaa"
+    #pw = "yjxkk131415"
     sender = 'xieke.buaa@gmail.com'
     receivers = ['cx28@cs.rutgers.edu']
 
-    message = msg
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = COMMASPACE.join(receivers)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = 'event list'
+
+    #message = msg
+    msg.attach( MIMEText('Please see attachment') )
+
+    file = 'monitor.sh'
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload( open(file,"rb").read() )
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
+    msg.attach(part)
+
+    print 'ok'
+
     try:
     #smtpObj = smtplib.SMTP('localhost')
     #smtpObj.sendmail(sender, receivers, message)
@@ -16,10 +38,10 @@ def send_email(msg):
         smtp.starttls()
         smtp.ehlo
         smtp.login('oeddyo@gmail.com', 'yueyue-0105&love')
-        smtp.sendmail(sender, receivers, message)
+        smtp.sendmail(sender, receivers, msg.as_string())
         print "Successfully sent email"
     except Exception, e:
         print str(e)
 
 
-send_email('hi')
+send_email()
